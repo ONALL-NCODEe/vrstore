3d7b6707ed11947539c7c18bd1d5251833b6f98c
Thạch Kim Va Na <vana.tkvn@gmail.com>
2024-10-22 02:04:09 +0700

    Category Management with CRUD #1

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 //utils
 import connectDB from "./config/db.js";
 import userRoutes from "./routers/userRoutes.js";
+import categoryRoutes from "./routers/categoryRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -19,5 +20,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
+app.use("/api/category", categoryRoutes);
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -167,7 +167,7 @@ const Navigation = () => {
                                         to="/admin/categorylist"
                                         className="block px-4 py-2 hover:bg-gray-100"
                                     >
-                                        Danh mục
+                                        Quản lý danh mục
                                     </Link>
                                 </li>
                                 <li>
