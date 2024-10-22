17461ef1d2b7ce8f82fff3cb653b1f360840260c
Thạch Kim Va Na <vana.tkvn@gmail.com>
2024-10-23 02:27:45 +0700

    Product Management with CRUD #1

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import userRoutes from "./routers/userRoutes.js";
 import categoryRoutes from "./routers/categoryRoutes.js";
+import productRoutes from "./routers/productRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -21,5 +22,6 @@ app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
